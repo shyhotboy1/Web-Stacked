14f8748bec86a418cdca3ff68cebcacb3ab05a96
shyhotboy1 <49177279+shyhotboy1@users.noreply.github.com>
2020-10-16 11:50:12 -0400

    Update script.js

--- WebStack/script.js
+++ WebStack/script.js
@@ -1,1 +1,9 @@
-
+const stack = new Stack();
+console.log(stack.size());
+console.log(stack.push("Adrian Martinez"));
+console.log(stack.push("Josue"));
+console.log(stack.size()); // 2
+stack.print(); // [Adrian Martinez, Josue]
+console.log(stack.peek()); // Josue
+console.log(stack.pop()); // Josue
+console.log(stack.size()); // Adrian Martinez
